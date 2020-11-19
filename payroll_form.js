52b4aa48e2c8396227c1789b658edca7adf987ec
nayan-bansal <nayan.7991@gmail.com>
2020-11-20 05:27:31 +0530

    Added: Ability to save employee object to local stoarage

--- payroll_form.js
+++ payroll_form.js
@@ -54,9 +54,21 @@ window.addEventListener('DOMContentLoaded',(event)=>{
 const save = () => {
     try{
         let employeePayrollData=createEmployeePayroll();
+        createAndUpdateStorage(employeePayrollData);
     }catch(e){
         return;
     }
+}
+
+function createAndUpdateStorage(employeePayrollData){
+    let employeePayrollList=JSON.parse(localStorage.getItem("EmployeePayrollList"));
+    if(employeePayrollData!=undefined){
+        employeePayrollList.push(employeePayrollData);
+    }else{
+        employeePayrollList=[employeePayrollData];
+    }
+    alert(employeePayrollList.toString())
+    localStorage.setItem("EmployeePayrollList",JSON.stringify(employeePayrollList));
 }
 
 const createEmployeePayroll=()=>{
