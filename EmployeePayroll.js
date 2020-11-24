a0988618b4c4b41deba5e15dd77e3a048d2abe50
nayan-bansal <nayan.7991@gmail.com>
2020-11-24 14:18:26 +0530

    Save Employee Data in Local Storage

---
+++ EmployeePayroll.js
@@ -0,0 +1,62 @@
+class EmployeePayroll {
+    get id() {
+        return this._id;
+    }
+    set id(id) {
+        this._id = id;
+    }
+    get name() {
+        return this._name;
+    }
+    set name(name) {
+        let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
+        if (nameRegex.test(name)) this._name = name;
+        else throw "Name is incorrect";
+    }
+    get profilePic() {
+        return this._profilePic;
+    }
+    set profilePic(profilePic) {
+        this._profilePic = profilePic;
+    }
+    get gender() {
+        return this._gender;
+    }
+    set gender(gender) {
+        this._gender = gender;
+    }
+    get salary() {
+        return this._salary;
+    }
+    set salary(salary) {
+        this._salary = salary;
+    }
+    get startDate() {
+        return this._startDate;
+    }
+    set startDate(startDate) {
+        let now = new Date();
+        if (startDate > now) throw 'Start Date is in future Date!';
+        var diff = Math.abs(now.getTime() - startDate.getTime());
+        if (diff / (1000 * 60 * 60 * 24) > 30) throw 'Start Date is beyond 30 days!';
+        this._startDate = startDate;
+    }
+    get department() {
+        return this._department;
+    }
+    set department(department) {
+        this._department = department;
+    }
+    get notes() {
+        return this._notes;
+    }
+    set notes(notes) {
+        this._notes = notes;
+    }
+
+    toString() {
+        const format = { year: "numeric", month: "short", day: "numeric" };
+        const date = this.startDate === undefined ? "undefined" : this.startDate.toLocaleDateString("en-IN", format);
+        return "id = " + this.id + ", name = " + this.name + ", profile Pic = " + this.profilePic + ", salary = " + this.salary + ", gender = " + this.gender + ", start date = " + date + ", department = " + this.department + ", notes = " + this.notes;
+    }
+}
